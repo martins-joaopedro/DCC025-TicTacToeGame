c8c8c642752a0130fb48226706aa2e56b938988b
João Pedro Martins Cruz <103667165+martins-joaopedro@users.noreply.github.com>
2024-07-18 15:44:42 -0300

    refactor: make better player creation and getting its values

--- src/main/java/br/ufjf/game/components/Game.java
+++ src/main/java/br/ufjf/game/components/Game.java
@@ -1,5 +1,7 @@
 package br.ufjf.game.components;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,72 +9,110 @@ public class Game {
 
     private boolean startPlayer;
     private Gamemode gamemode;
-    private Player player1;
-    private Player player2;
+    private List<Player> players;
     //private Bot bot;
     private Grid grid;
     private Menu menu;
-    private Scanner scanner;
 
     public Game(Scanner scanner) {
         // randomize who starts playing
         Random random = new Random();
         this.startPlayer = random.nextInt() % 2 == 0;
 
+        this.players = new ArrayList<Player>();
         this.grid = new Grid();
         this.menu = new Menu(scanner);
-        this.scanner = scanner;
     }
 
     public void start() {
-
         menu.welcome();
         this.gamemode = menu.askGamemode();
 
-        createPlayers();
+        if(gamemode == Gamemode.SINGLEPLAYER) {
+            createFirstPlayer();
+            alternatePlayer();
+            createSecondPlayer();
+        } else {
+            createFirstPlayer();
+            initBot();
+        }
+
         gameLoop();
     }
 
-    private void createPlayers() {
+    private void createFirstPlayer() {
 
-        if(gamemode == Gamemode.SINGLEPLAYER) {
-            String name = menu.askPlayerName(1);
-            String symbol = menu.askPlayerSymbol(1);
-            player1 = new Player(name, symbol);
+        int playerIndex = getCurrentPlayerIndex();
 
-            name = menu.askPlayerName(2);
-            symbol = menu.askPlayerSymbol(2);
-            player2 = new Player(name, symbol);
-        }
+        String name = menu.askPlayerName(playerIndex);
+        String symbol = menu.askPlayerSymbol(playerIndex);
+
+        players.add(new Player(name, symbol));
+    }
+
+    private void createSecondPlayer() {
+
+        int playerIndex = getCurrentPlayerIndex();
+
+        String name = menu.askPlayerName(playerIndex);
+        String symbol = players.get(0).getSymbol();
+        String newSymbol = symbol == "X" ? "O" : "X";
+
+        System.out.println("Seu símbolo foi definido como: " + newSymbol);
+
+        players.add(new Player(name, newSymbol));
+    }
+
+    private int getCurrentPlayerIndex() {
+        if(startPlayer)
+            return 0;
+        else return 1;
+    }
+
+    private void initBot() {
+
     }
 
     public void startRound() {
 
+        int playerIndex = getCurrentPlayerIndex();
+        System.out.println("Jogador " + playerIndex + ": " + players.get(playerIndex).getName());
+
+        int x, y;
+        do {
+            x = menu.askPosition("x");
+            y = menu.askPosition("y");
+        } while(!grid.isValidCoords(x, y));
+
         if(startPlayer)
-            System.out.println("Jogador 1: " + player1.getName());
-        else System.out.println("Jogador 2: " + player2.getName());
-
-        System.out.println("Digite uma coordenada x: ");
-        int x = scanner.nextInt();
-
-        System.out.println("Digite uma coordenada y: ");
-        int y = scanner.nextInt();
-
-        if(startPlayer)
-            grid.setCellValue(x, y, player1.getSymbol());
-        else grid.setCellValue(x, y, player2.getSymbol());
+            grid.setCellValue(x, y, players.get(0).getSymbol());
+        else grid.setCellValue(x, y, players.get(1).getSymbol());
     }
 
-    public void nextRound() {
-
+    public void alternatePlayer() {
         this.startPlayer = !this.startPlayer;
     }
 
+    private void showWinner() {
+        int playerIndex = getCurrentPlayerIndex();
+        System.out.println("Parabens, " + players.get(playerIndex).getName() + " você ganhou o jogo!");
+    }
+
     private void gameLoop() {
-        while(!this.grid.isGameOver()) {
+
+        boolean isGameOver;
+
+        do {
             startRound();
             grid.printGrid();
-            nextRound();
-        }
+            isGameOver = this.grid.isGameOver();
+
+            if(!isGameOver)
+                alternatePlayer();
+        } while(!isGameOver);
+
+        if(isGameOver)
+            showWinner();
+        else System.out.println("Deu velha!!!");
     }
 }
